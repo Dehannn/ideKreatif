5e6873bad16f3ab11dcd4923dde7275864efa34b
Dehannn <muhammad.pasha3310@smk.belajar.id>
2025-02-02 19:54:26 +0700

    menambahkan edit data kategori

--- kategori.php
+++ kategori.php
@@ -57,31 +57,55 @@ include '.includes/toast_notification.php';
                         </td>
                     </tr>
                     <!-- modal untuk hapus data kategori -->
-<div class="modal fade" id="deleteCategory_<?= $category['category_id']; ?>" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Hapus Kategori?</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-            <div class="modal-body">
-                <form action="proses_kategori.php" method="POST">
-                    <div>
-                        <p>Tindakan ini tidak bisa dibatalkan.</p>
-                        <input type="hidden" name="catID" value="<?=$category['category_id']; ?>">
+                    <div class="modal fade" id="deleteCategory_<?= $category['category_id']; ?>" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Hapus Kategori?</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="proses_kategori.php" method="POST">
+                                        <div>
+                                            <p>Tindakan ini tidak bisa dibatalkan.</p>
+                                            <input type="hidden" name="catID" value="<?=$category['category_id']; ?>">
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-outline-secondary"
+                                            data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-secondary"
-                        data-bs-dismiss="modal">Batal</button>
-                        <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
+                    <!-- modal untuk update data kategori -->
+                    <div id="editCategory_<?= $category['category_id']; ?>" class="modalfade" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Update Data Kategori</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="proses_kategori.php" method="POST">
+                                        <!-- Input tersembunyi untuk menyimpan ID kategori -->
+                                        <input type="hidden" name="catID" value="<?=$category['category_id']; ?>">
+                                        <div class="form-group">
+                                            <label>Nama Kategori</label>
+                                            <!-- Input untuk nama kategori -->
+                                            <input type="text" value="<?= $category['category_name']; ?>" name="category_name" class="form-control">
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" name="update" class="btn btn-warning">Update</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- modal untuk update data kategori -->
-                    
                         <?php endwhile; ?>
                     </tbody>
                 </table>
